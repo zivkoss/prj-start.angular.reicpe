6b4620a0c5af3e14a6ec648795e309f554518ba7
zivkoss <zivcoss@yahoo.com>
2023-05-24 19:12:20 +0200

    recipe-edit.c.html line // added

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -29,6 +29,10 @@ export class RecipeEditComponent implements OnInit {
        );
   }
 
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
