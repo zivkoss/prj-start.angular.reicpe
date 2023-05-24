da384f0fa11f2a7081ddb23be1044df465cd8c78
zivkoss <zivcoss@yahoo.com>
2023-05-24 16:20:31 +0200

    shopping-edit.componenet.ts line 52 this.editMode = false;     form.reset(); added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -41,18 +41,20 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
       this.slService.updateIngredient(this.editedItemIndex, newIngredient)
     } else {
       this.slService.addIngredient(newIngredient);
-
     }
+    this.editMode = false;
+    form.reset();
   }
    
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 }
+
